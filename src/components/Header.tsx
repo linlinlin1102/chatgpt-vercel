e6bf433fb0941040a3790aba767ccfc9ab25e5b7
li lin jin <65350702+linlinlin1102@users.noreply.github.com>
2023-05-04 15:47:23 +0800

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -69,19 +69,6 @@ export default function Header() {
                   class="ml-2 <sm:hidden"
                   href="https://github.com/ourongxing/chatgpt-vercel"
                 >
-                  <img
-                    alt="GitHub forks badge"
-                    src="https://img.shields.io/github/stars/ourongxing/chatgpt-vercel?logo=github"
-                  />
-                </a>
-                <a
-                  class="ml-2"
-                  href="https://github.com/ourongxing/chatgpt-vercel/fork"
-                >
-                  <img
-                    alt="GitHub forks badge"
-                    src="https://img.shields.io/github/forks/ourongxing/chatgpt-vercel?logo=github"
-                  />
                 </a>
               </>
             }
